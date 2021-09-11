16e556c03886983974ae4222648828ef39c5b5c3
Sasikanth <sasikanth@Sasikanths-MacBook-Pro.local>
2021-09-11 19:05:19 +0530

    Updated arguments

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ from config import *
 
 
 parser = argparse.ArgumentParser()
+'''
 parser.add_argument("-l","--lambda_loss",type=float, default=1.2, 
                     help="Adjustment graph loss parameter between the labeled and unlabeled")
 parser.add_argument("-s","--s_margin", type=float, default=0.1,
@@ -44,6 +45,27 @@ parser.add_argument("-c","--cycles", type=int, default=5,
                     help="Number of active learning cycles")
 parser.add_argument("-t","--total", type=bool, default=False,
                     help="Training on the entire dataset")
+'''
+
+
+parser.add_argument('--lambda_loss',type=float, default=1.2, 
+                    help='Adjustment graph loss parameter between the labeled and unlabeled')
+parser.add_argument('--s_margin', type=float, default=0.1,
+                    help='Confidence margin of graph')
+parser.add_argument('--hidden_units', type=int, default=128,
+                    help='Number of hidden units of the graph')
+parser.add_argument('--dropout_rate', type=float, default=0.3,
+                    help='Dropout rate of the graph neural network')
+parser.add_argument('--dataset', type=str, default='cifar100',
+                    help='')
+parser.add_argument('--no_of_epochs', type=int, default=20,
+                    help='Number of epochs for the active learner')
+parser.add_argument('--method_type', type=str, default='lloss',
+                    help="")
+parser.add_argument('--cycles', type=int, default=5,
+                    help='Number of active learning cycles')
+parser.add_argument('--total', type=bool, default=False,
+                    help='Training on the entire dataset')
 
 args = parser.parse_args()
 
